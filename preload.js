b1b5bf99c67aec24f2a2352abd0e7ec7755cbe7c
alonharazi3-web <alonharazi3@gmail.com>
2026-03-31 21:58:37 +0300

    deploy:  (2026-03-31 21:58)

--- preload.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Triggers native OAuth popup window, returns { token, expiresIn }
-  oauthSignIn: (authUrl, redirectUri) =>
-    ipcRenderer.invoke('oauth-signin', authUrl, redirectUri)
-});
